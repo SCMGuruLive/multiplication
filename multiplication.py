112a62ac3c849fa3465d595ff83783fcae141538
Scott C. MacCallum <scm@pdp10.org>
2020-12-28 21:42:04 -0500

    Added fact testing and so much more

--- multiplication.py
+++ multiplication.py
@@ -20,166 +20,252 @@
 
 def facts():
     print('')
+
     multiplicand = int(input('What facts do you want to see? '))
+
     for multiplier in range(1,13):
         product = multiplicand * multiplier
         print('')
         print(f'{multiplicand} x {multiplier} = {product}')
 
+def test_ordered_range():
+    print('')
+
+    print('')
+    
+    multiplicand = int(input('What facts do you want to test? '))
+
+    for multiplier in range(1,13):
+        product = multiplicand * multiplier
+        print('')
+        print(f'What is {multiplicand} x {multiplier}? ', end='')
+        answer = input()
+        answer = int(answer)
+        if answer == product:
+            print('Correct!')
+        else:
+            print('Not correct.')
+
+# def test_random_range():
+
+# def test_random_all():
+
 def table():
     print('')
+
     print('   ', end='')
+
     for multiplier in range(1,9):
         print(f' {multiplier} ', end='')
+
     for multiplier in range(9,10):
         print(f'  {multiplier} ', end='')
+
     for multiplier in range(10,13):
         print(f' {multiplier} ', end='')
 
-
     print('')
+
     print('  x', end='')
+
     for multiplier in range(1,40):
         print('_', end='')
 
     print('')
+
     print(' 2| ', end='')
+
     multiplicand = 2
+
     for multiplier in range(1,3):
         product = multiplicand * multiplier
         print(f'{product}  ', end='')
+
     for multiplier in range(3,4):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(4,9):
         product = multiplicand * multiplier
         print(f' {product}', end='')
+
     for multiplier in range(9,13):
         product = multiplicand * multiplier
         print(f'  {product}', end='')
 
     print('')
+
     multiplicand = 3
+
     print(' 3| ', end='')
+
     for multiplier in range(1,3):
         product = multiplicand * multiplier
         print(f'{product}  ', end='')
+
     for multiplier in range(3,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,13):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
 
     print('')
+
     multiplicand = 4
+
     print(' 4| ', end='')
+
     for multiplier in range(1,2):
         product = multiplicand * multiplier
         print(f'{product}  ', end='')
+
     for multiplier in range(2,8):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(8,13):
         product = multiplicand * multiplier
         print(f'{product}  ', end='')
 
     print('')
+
     multiplicand = 5
+
     print(' 5| ', end='')
+
     for multiplier in range(1,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,13):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
 
     print('')
+
     multiplicand = 6
+
     print(' 6| ', end='')
+
     for multiplier in range(1,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,13):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
 
     print('')
+
     multiplicand = 7
+
     print(' 7| ', end='')
+
     for multiplier in range(1,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,13):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
 
     print('')
+
     multiplicand = 8
+
     print(' 8| ', end='')
+
     for multiplier in range(1,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,13):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
 
     print('')
+
     multiplicand = 9
+
     print(' 9| ', end='')
+
     for multiplier in range(1,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,11):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
+
     for multiplier in range(11,13):
         product = multiplicand * multiplier
         print(f' {product}', end='')
 
     print('')
+
     multiplicand = 10
+
     print('10|', end='')
+
     for multiplier in range(1,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,10):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
+
     for multiplier in range(10,11):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(11,13):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
 
     print('')
+
     multiplicand = 11
+
     print('11|', end='')
+
     for multiplier in range(1,9):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(9,10):
         product = multiplicand * multiplier
         print(f' {product} ', end='')
+
     for multiplier in range(10,13):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
 
     print('')
+
     multiplicand = 12
+
     print('12|', end='')
+
     for multiplier in range(1,10):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
+
     for multiplier in range(10,13):
         product = multiplicand * multiplier
         print(f'{product} ', end='')
 
 def main():
     facts()
+
     table()
+
+    test_ordered_range()
+
     print('')
+
     print('')
 
 if __name__ == "__main__":
